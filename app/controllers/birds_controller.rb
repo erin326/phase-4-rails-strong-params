d7cf649c264de694811bbde19e79c9339adae529
erin <erinverkoulen93@gmail.com>
2021-10-29 15:07:04 -0500

    commit tree 51e8f90f1878010a9a3a4025fed4ec1693277225

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  # wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -8,7 +8,13 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+
+    # creates security issue (mass assignment vulnerabilty):
+    # bird = Bird.create(params)
+
+    #strong parameters: whitelist only the parameters we want to use:
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +26,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private 
+
+  def bird_params 
+    params.permit(:name, :species)
   end
 
 end
